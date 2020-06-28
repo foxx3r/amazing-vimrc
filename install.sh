c5299fc53dfd358a8e9109be27609a5556b305b2
Dheisom Gomes <dheisom@debian>
2020-06-28 12:14:02 -0300

    Melhoraei o instalador para ficar mais flexível e corrigi um erro que acontecia quando você instala como root e deois como usuário comum

--- install.sh
+++ install.sh
@@ -1,65 +1,74 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+echo "Usuário atual: `whoami`"
 echo "ATENÇÃO: você precisa ser root para executar este script, a menos que você esteja usando termux"
-sleep 3
 
-echo "instalando o neovim..."
-
-if [ -e "/bin/apt-get" ]
-then
-    apt-get update > /dev/null
-    apt-get install neovim -y > /dev/null
-    apt-get install nodejs -y > /dev/null
-    apt-get install npm -y > /dev/null
-elif [ -e "/bin/dnf" ]
-then
-    dnf check-update
-    dnf install neovim
-    dnf install nodejs
-elif [ -e "/bin/apk" ]
-then
-    apk update > /dev/null
-    apk add neovim -y > /dev/null
-    apk add nodejs -y > /dev/null
-elif [ -e "/bin/pacman" ]
-then
-    pacman -Sy > /dev/null
-    pacman -S neovim
-    pacman -S nodejs
-elif [ -e "/data/data/com.termux/files/usr/bin/apt-get" ]
-then
-    apt-get update > /dev/null
-    apt-get install neovim -y > /dev/null
-    apt-get install nodejs -y > /dev/null
+if [ "`whoami`" == "root" ];then
+    read -p "Instalar programas extras para melhorar ainda mais o NeoVim?[s/n] " extra
+else
+    extra="n"
+fi
+if [ "$extra" == "s" -o "$extra" == "S" ];then
+    programs="nodejs curl neovim git"
+    extra="s"
+else
+    programs="curl neovim git"
+    extra="n"
 fi
 
-if [ -d "$HOME/.config/nvim" ]
-then
+echo "instalando os programas..."
+if [ "`whoami`" != "root" ];then
+    echo "Pulando instalações por falta de root..."
+elif [ -e "/bin/apt-get" ];then
+    apt-get update
+    apt-get install $programs -y
+    if [ "$extra" == "s" ];then
+        echo "Instalando o npm..."
+        apt-get install npm -y
+    fi
+elif [ -e "/bin/dnf" ];then
+    dnf check-update
+    dnf install $programs
+elif [ -e "/bin/apk" ];then
+    apk update
+    apk add $programs -y
+    if [ "$extra" == "s" ];then
+        apk add npm -y
+    fi
+elif [ -e "/bin/pacman" ];then
+    pacman -Sy
+    pacman -S $programs
+elif [ -e "/data/data/com.termux/files/usr/bin/apt-get" ];then
+    apt-get update
+    apt-get install $programs -y
+fi
+
+if [ -d "$HOME/.config/nvim" ];then
     rm -rf ~/.config/nvim
 else
     mkdir -p ~/.config/nvim
 fi
 
-git clone https://github.com/foxx3r/amazing-vimrc ~/.config/nvim 2> /dev/null
+git clone https://github.com/foxx3r/amazing-vimrc ~/.config/nvim
 
-if [ -e "/data/data/com.termux/files/usr/bin/apt-get" ]
-then
-    mkdir $PREFIX/var/tmp/nvim/backups -p
+if [ -e "/data/data/com.termux/files/usr/bin/apt-get" ];then
+    mkdir -p $PREFIX/var/tmp/nvim/backups $PREFIX/var/tmp/nvim/swaps/
     echo "\" BACKUP" >> ~/.config/nvim/init.vim
-    echo "set backup" >> ~/.config/nvim/init.vim
     echo "set writebackup" >> ~/.config/nvim/init.vim
-    echo "set backupdir=$PREFIX/var/tmp/nvim/backups" >> ~/.config/nvim/init.vim
-    echo "set dir=$PREFIX/var/tmp/nvim/swaps" >> ~/.config/nvim/init.vim
+    echo "set backupdir=.,$PREFIX/var/tmp/nvim/backups" >> ~/.config/nvim/init.vim
+    echo "set dir=.,$PREFIX/var/tmp/nvim/swaps" >> ~/.config/nvim/init.vim
 else
-    mkdir /var/tmp/nvim/backups -p
+    mkdir -p ~/.local/share/nvim/backup ~/.local/share/nvim/swaps/
     echo "\" BACKUP" >> ~/.config/nvim/init.vim
-    echo "set backupdir" >> ~/.config/nvim/init.vim
-    echo "set writebackup" >> ~/.config/nvim/init.vim 
-    echo "set backupdir=/var/tmp/nvim/backups" >> ~/.config/nvim/init.vim 
-    echo "set dir=/var/tmp/nvim/swaps" >> ~/.config/nvim/init.vim
+    echo "set writebackup" >> ~/.config/nvim/init.vim
+    echo "set backupdir=.,~/.local/share/nvim/backup" >> ~/.config/nvim/init.vim
+    echo "set dir=.,~/.local/share/nvim/swaps" >> ~/.config/nvim/init.vim
 fi
 
-echo "Instalando dependências adicionais..."
-npm i -g yarn
-yarn global add vim-language-server
+if [ "$extra" == "s" ];then
+    echo "Instalando dependências adicionais..."
+    npm i -g yarn
+    yarn global add vim-language-server
+fi
 
 echo "pronto! agora entre no NeoVim digitando 'nvim' e rode :PlugInstall (ou :PlugI, ele auto-completa pra gente :))"
